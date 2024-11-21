32c3ad5ecbdbf208cead98c0906ba7c326efdd3b
Elodie LIM <elodie.lim@edu.esiee.fr>
2024-11-22 00:13:21 +0100

    update avec un bras casse

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,4 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+**La fct a pour but de copier un bloc de memoire specifie
+**par le @param source dont la taille est specifiee par @param size
+**dans un nouvel emplacement designe par le @param dest
+*/
 
+//#include "libft.h"
+#include <stddef.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+
+	if (!dst && !src)
+		return (0);
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+
+	return (dst);
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+int main(void)
+{
+    char src[] = "test";
+    char dest[10];
+
+    ft_memcpy(dest, src, 5);
+
+    printf("source: %s\n", src);
+    printf("destination: %s\n", dest);
+
+    return 0;
+}*/
---
+++ ft_memmove.c
@@ -0,0 +1,7 @@
+/*
+**La fct memmove() copie n octets depuis la zone memoire src 
+**vers la zone memoire dest.
+**@param void *dest
+**@param const void *src
+**@param size_t n
+*/
